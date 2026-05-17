c91aea10a5d135d810ded6a059113f9bbb69675f
conolas <conolas.de.arts@gmail.com>
2026-05-17 21:40:51 +0530

    added placement

---
+++ openroad/placement/3_2_place_iop.tcl
@@ -0,0 +1,25 @@
+#Edge: BOTTOM
+#Edge: RIGHT
+place_pin -pin_name rst_n -layer met3 -location {99.6 22.1} -force_to_die_boundary
+place_pin -pin_name rx_data[0] -layer met3 -location {99.6 71.06} -force_to_die_boundary
+place_pin -pin_name rx_data[1] -layer met3 -location {99.6 61.54} -force_to_die_boundary
+place_pin -pin_name rx_data[2] -layer met3 -location {99.6 49.3} -force_to_die_boundary
+place_pin -pin_name rx_data[3] -layer met3 -location {99.6 41.14} -force_to_die_boundary
+place_pin -pin_name rx_data[4] -layer met3 -location {99.6 64.26} -force_to_die_boundary
+place_pin -pin_name rx_data[5] -layer met3 -location {99.6 56.1} -force_to_die_boundary
+place_pin -pin_name rx_data[6] -layer met3 -location {99.6 54.74} -force_to_die_boundary
+place_pin -pin_name rx_data[7] -layer met3 -location {99.6 34.34} -force_to_die_boundary
+place_pin -pin_name rx_done -layer met3 -location {99.6 68.34} -force_to_die_boundary
+#Edge: TOP
+place_pin -pin_name tx_busy -layer met2 -location {40.71 99.757} -force_to_die_boundary
+place_pin -pin_name tx_data[0] -layer met2 -location {29.67 99.757} -force_to_die_boundary
+place_pin -pin_name tx_data[4] -layer met2 -location {31.51 99.757} -force_to_die_boundary
+place_pin -pin_name tx_data[5] -layer met2 -location {25.99 99.757} -force_to_die_boundary
+place_pin -pin_name tx_data[6] -layer met2 -location {20.47 99.757} -force_to_die_boundary
+place_pin -pin_name tx_data[7] -layer met2 -location {21.39 99.757} -force_to_die_boundary
+#Edge: LEFT
+place_pin -pin_name clk -layer met3 -location {0.4 86.02} -force_to_die_boundary
+place_pin -pin_name tx_data[1] -layer met3 -location {0.4 69.7} -force_to_die_boundary
+place_pin -pin_name tx_data[2] -layer met3 -location {0.4 72.42} -force_to_die_boundary
+place_pin -pin_name tx_data[3] -layer met3 -location {0.4 73.78} -force_to_die_boundary
+place_pin -pin_name tx_start -layer met3 -location {0.4 57.46} -force_to_die_boundary
